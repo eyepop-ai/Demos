3e98c87e3725c14b56e595c1456d058810ae1528
Andy <andrewballester@gmail.com>
2024-01-26 15:16:34 -0800

    Refactor for new auth

---
+++ AI Photo Overlay - Low code Tester/config.js
@@ -0,0 +1,9 @@
+const pop_endpoint = "" 
+
+// Do not expose your API key in production! You can leave this blank in development.
+const api_key = ''
+
+// You can create a temporary token for your users to authenticate with. Keeping this blank in development will allow you to login to authenticate.
+// https://docs.eyepop.ai/developer-documentation/api-key
+const token = '' 
+config={};
