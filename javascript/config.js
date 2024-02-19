5e764eb828a2be3efd25bab767f85b40f5cea979
Edmund Dao <61922101+64blit@users.noreply.github.com>
2024-02-18 18:41:18 -0800

    Merged all config files one directory up to simplify running demos.

--- javascript/config.js
+++ javascript/config.js
@@ -1,8 +1,8 @@
-const pop_uuid = '';
+const pop_uuid = 'f10ac5dcb812495b9028332dc96922a7';
 const pop_endpoint = "https://api.eyepop.ai/pops/" + pop_uuid + "/config?auto_start=true";
 
 // Do not expose your API key in production! You can leave this blank in development.
-const api_key = '';
+const api_key = 'AAExPucBgeT1wZQtB967MI7nZ0FBQUFBQmwwcXExNkJuODhyNUVGbUd0Z0RJSUx0SE0ySkpqREQ3REFBbEYtazZvV0lRU3dLZ05naXcwN0Y1YUtMTjJWUl9jV04zOUxNZHNoWE10d1IxcXhwVzZLVXg2RTJDc0txSXllSXg3SXV2OEtRaG5rem89';
 
 // You can create a temporary token for your users to authenticate with. Keeping this blank in development will allow you to login to authenticate.
 // https://docs.eyepop.ai/developer-documentation/api-key
