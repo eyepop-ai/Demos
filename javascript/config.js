a9f79aff34b0bb32640776e81d9929a1fdde5d94
Edmund Dao <61922101+64blit@users.noreply.github.com>
2024-09-12 15:39:40 -0700

    Update config.js

--- javascript/config.js
+++ javascript/config.js
@@ -1,8 +1,8 @@
-const pop_uuid = 'e4fd9369a9de42f6becfb90e11f4620c';
+const pop_uuid = '';
 const pop_endpoint = "https://api.eyepop.ai/pops/" + pop_uuid + "/config?auto_start=true";
 
 // Do not expose your API key in production! You can leave this blank in development.
-const api_key = 'AAFWTEvZaWw-h40Bga2N9swqZ0FBQUFBQmwyU3JzOXNSblFFaWZVR2R4aEpyY0hlSmVDRzJOUFJsUE9QQ0lUUkI2YURpYXFPYWF5emQxdkc1ekplc3JMcW45T3JHN1VzTnlBUHp3NDJJeVlvcHg0UHFxOVUzWS1tR25GZ3V3eXZZMy0zcmlHT009';
+const api_key = '';
 
 // You can create a temporary token for your users to authenticate with. Keeping this blank in development will allow you to login to authenticate.
 // https://docs.eyepop.ai/developer-documentation/api-key
