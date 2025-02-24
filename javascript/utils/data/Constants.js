e1c50f96af304647c6a65121c342d5688daf2f5d
Andy Ballester <andrewballester@gmail.com>
2025-02-24 10:47:41 -0800

    Removed outdated Javascript SDK examples

--- javascript/utils/data/Constants.js
+++
@@ -1,8 +0,0 @@
-
-// a state enum for the people
-export const PeopleState = Object.freeze({
-    DETECTED: Symbol("detected"),
-    TRACKING: Symbol("tracking"),
-    LOST: Symbol("lost"),
-    REMOVED: Symbol("removed"),
-});
